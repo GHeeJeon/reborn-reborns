240b091a23489023ff43a90f3ea7ccdc707a68b7
GHeeJeon <jhjeon_99@naver.com>
2023-05-04 03:00:32 +0900

    vue-add voca page

---
+++ src/pages/vocabulary/vocabulary.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="pt-4 px-4">
+    <div class="flex gap-2 gap-x-12">
+      <VCard block>
+        <template #media>
+          <img :src="landscape" alt="">
+        </template>
+        <template #header>Hills </template>
+        <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+          Cupiditate aspernatur unde harum perferendis<br>
+          suscipit numquam commodi aut quia consequatur, doloremque<br>
+          ab beatae aliquam impedit sit quos praesentium<br>
+          sequi sed fugiat.
+
+        </p>
+      </VCard>
+    </div>
+  </div>
+</template>
+
+<script>
+useHead({
+  title: "vocabulary",
+});
+export default {
+  name: "vocabulary"
+}
+</script>
+
+<style scoped>
+
+</style>
+<route lang="yaml">
+meta:
+layout: home
+</route>
